c213f98e3c1d1c67f3fedf130b99970c3720d797
SanctumSpear <146847788+SanctumSpear@users.noreply.github.com>
2026-04-11 19:40:26 -0400

    Restore old main code in comment

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,6 +1,30 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "client.h"
+
+//int main() {
+//    try {
+//        Client client("127.0.0.1", 27500);
+//        std::cout << "Connected to server!\n";
+//
+//        // Send a message to the server
+//        client.SendString("Hello from client!");
+//        std::cout << "Client: Hello from client!\n";
+//
+//        // Receive the server's response
+//        std::string received = client.ReceiveString();
+//        std::cout << "Server: " << received << "\n";
+//
+//        client.Cleanup();
+//    }
+//    catch (const std::exception& e) {
+//        std::cerr << "Client error: " << e.what() << "\n";
+//        return 1;
+//    }
+//
+//    return 0;
+//}
 
 int main() {
     if (!glfwInit()) {
